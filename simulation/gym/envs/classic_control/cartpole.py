6cf9e538657896433412afbe94db2a2b983946a7
wontothree <prepared7913@naver.com>
2024-09-06 15:55:34 +0900

    sim sev

--- simulation/gym/envs/classic_control/cartpole.py
+++ simulation/gym/envs/classic_control/cartpole.py
@@ -98,7 +98,7 @@ class CartPoleEnv(gym.Env[np.ndarray, Union[int, np.ndarray]]):
 
         # Angle at which to fail the episode
         self.theta_threshold_radians = 12 * 2 * math.pi / 360
-        self.x_threshold = 0.36
+        self.x_threshold = 1
 
         # Angle limit set to 2 * theta_threshold_radians so failing observation
         # is still within bounds.
@@ -137,17 +137,78 @@ class CartPoleEnv(gym.Env[np.ndarray, Union[int, np.ndarray]]):
 
         self.steps_beyond_terminated = None
 
+    # def step(self, action):
+    #     err_msg = f"{action!r} ({type(action)}) invalid"
+    #     assert self.action_space.contains(action), err_msg
+    #     assert self.state is not None, "Call reset before using step method."
+    #     x, x_dot, theta, theta_dot = self.state
+    #     force = self.force_mag if action == 1 else -self.force_mag
+    #     costheta = math.cos(theta)
+    #     sintheta = math.sin(theta)
+
+    #     # For the interested reader:
+    #     # https://coneural.org/florian/papers/05_cart_pole.pdf
+    #     temp = (
+    #         force + self.polemass_length * theta_dot**2 * sintheta
+    #     ) / self.total_mass
+    #     thetaacc = (self.gravity * sintheta - costheta * temp) / (
+    #         self.length * (4.0 / 3.0 - self.masspole * costheta**2 / self.total_mass)
+    #     )
+    #     xacc = temp - self.polemass_length * thetaacc * costheta / self.total_mass
+
+    #     if self.kinematics_integrator == "euler":
+    #         x = x + self.tau * x_dot
+    #         x_dot = x_dot + self.tau * xacc
+    #         theta = theta + self.tau * theta_dot
+    #         theta_dot = theta_dot + self.tau * thetaacc
+    #     else:  # semi-implicit euler
+    #         x_dot = x_dot + self.tau * xacc
+    #         x = x + self.tau * x_dot
+    #         theta_dot = theta_dot + self.tau * thetaacc
+    #         theta = theta + self.tau * theta_dot
+
+    #     self.state = (x, x_dot, theta, theta_dot)
+
+    #     terminated = bool(
+    #         x < -self.x_threshold
+    #         or x > self.x_threshold
+    #         # or theta < -self.theta_threshold_radians
+    #         # or theta > self.theta_threshold_radians
+    #     )
+
+    #     if not terminated:
+    #         reward = 1.0
+    #     elif self.steps_beyond_terminated is None:
+    #         # Pole just fell!
+    #         self.steps_beyond_terminated = 0
+    #         reward = 1.0
+    #     else:
+    #         if self.steps_beyond_terminated == 0:
+    #             logger.warn(
+    #                 "You are calling 'step()' even though this "
+    #                 "environment has already returned terminated = True. You "
+    #                 "should always call 'reset()' once you receive 'terminated = "
+    #                 "True' -- any further steps are undefined behavior."
+    #             )
+    #         self.steps_beyond_terminated += 1
+    #         reward = 0.0
+
+    #     if self.render_mode == "human":
+    #         self.render()
+    #     return np.array(self.state, dtype=np.float32), reward, terminated, False, {}
+
     def step(self, action):
         err_msg = f"{action!r} ({type(action)}) invalid"
         assert self.action_space.contains(action), err_msg
         assert self.state is not None, "Call reset before using step method."
         x, x_dot, theta, theta_dot = self.state
-        force = self.force_mag if action == 1 else -self.force_mag
+
+        # 연속적인 action 값을 사용하여 force 설정
+        force = action[0] * self.force_mag  # action[0]이 -1.0과 1.0 사이의 값을 가짐
+
         costheta = math.cos(theta)
         sintheta = math.sin(theta)
 
-        # For the interested reader:
-        # https://coneural.org/florian/papers/05_cart_pole.pdf
         temp = (
             force + self.polemass_length * theta_dot**2 * sintheta
         ) / self.total_mass
@@ -196,6 +257,7 @@ class CartPoleEnv(gym.Env[np.ndarray, Union[int, np.ndarray]]):
         if self.render_mode == "human":
             self.render()
         return np.array(self.state, dtype=np.float32), reward, terminated, False, {}
+
 
     def reset(
         self,
