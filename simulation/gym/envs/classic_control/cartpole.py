f56c22379bd58a55ea3dc4900bfc672bbaad161b
wontothree <prepared7913@naver.com>
2024-09-06 14:51:45 +0900

    amend sim_env

--- simulation/gym/envs/classic_control/cartpole.py
+++ simulation/gym/envs/classic_control/cartpole.py
@@ -86,19 +86,19 @@ class CartPoleEnv(gym.Env[np.ndarray, Union[int, np.ndarray]]):
     }
 
     def __init__(self, render_mode: Optional[str] = None):
-        self.gravity = 9.8
-        self.masscart = 1.0
-        self.masspole = 0.1
+        self.gravity = 9.81
+        self.masscart = 0.123
+        self.masspole = 0.089
         self.total_mass = self.masspole + self.masscart
-        self.length = 0.5  # actually half the pole's length
+        self.length = 0.2  # actually half the pole's length
         self.polemass_length = self.masspole * self.length
-        self.force_mag = 10.0
+        self.force_mag = 1.0
         self.tau = 0.01 # 0.02  # seconds between state updates
         self.kinematics_integrator = "euler"
 
         # Angle at which to fail the episode
         self.theta_threshold_radians = 12 * 2 * math.pi / 360
-        self.x_threshold = 2.4
+        self.x_threshold = 0.36
 
         # Angle limit set to 2 * theta_threshold_radians so failing observation
         # is still within bounds.
