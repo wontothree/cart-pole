2891f442c277e8f11ee74089d8e9344dd28d5d6b
wontothree <prepared7913@naver.com>
2024-09-21 22:13:46 +0900

    Minor update

--- arduino-control/timer.cpp
+++ arduino-control/timer.cpp
@@ -1,6 +1,5 @@
 #include "timer.hpp"
 #include <avr/interrupt.h>
-#include <avr/io.h>
 
 void initializeTimer(uint8_t prescaler)
 {
--- arduino-control/timer.hpp
+++ arduino-control/timer.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <stdint.h>
+#include <avr/io.h>
 
 void initializeTimer(uint8_t prescaler);
 #define getTimerCount() ((uint16_t)TCNT1)
