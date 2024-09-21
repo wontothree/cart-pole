88894e6d27b087eb51c1f6e9d7d0455587088d5b
wontothree <prepared7913@naver.com>
2024-09-21 23:51:21 +0900

    Implement logging

--- arduino-control/arduino-control.ino
+++ arduino-control/arduino-control.ino
@@ -5,7 +5,7 @@
 
 void setup()
 {
-  initializeUart(500000);
+  initializeUart(1000000);
   initializeStepperMotorPins();
   initializeTimer(64);
 
@@ -13,23 +13,74 @@ void setup()
   float currentVelocity = 0;
   float currentPosition = 0;
 
-  uint16_t loggingInterval = 1000;
+  uint16_t loggingInterval = 2500;
   uint16_t lastLogging = 0;
-  char vBuf[16];
-  char pBuf[16];
-  char buf[64];
+
+  int sign = 1;
+  int num = 0;
+  uint8_t byte;
+
+  int16_t tmp;
 
   while (true)
   {
     uint16_t currentCount = getTimerCount();
 
+    // Motor control
     updateMotorByAcceleration(currentCount, acceleration, &currentVelocity, &currentPosition);
+
+    // Command input
+    if (getByte(&byte))
+    {
+      if (byte == '\n' || byte == '\r')
+      {
+        acceleration = sign * num * 0.01f;
+        // sprintf(buf, "Accel:%d/100\n", sign * num);
+        // putString(buf);
+        sign = 1;
+        num = 0;
+      }
+      else if (byte >= '0' && byte <= '9')
+      {
+        num *= 10;
+        num += (byte - '0');
+      }
+      else if (byte == '-')
+      {
+        sign = -1;
+      }
+    }
+
+    // Log output
     if (currentCount - lastLogging > loggingInterval)
     {
-      dtostrf(currentVelocity, 5, 3, vBuf);
-      dtostrf(currentPosition, 5, 3, pBuf);
-      sprintf(buf, "V:%s,P:%s\n", vBuf, pBuf);
-      putString(buf);
+      putByte('V');
+      tmp = currentVelocity * 10000;
+      if (tmp < 0)
+      {
+        putByte('-');
+        tmp *= -1;
+      }
+      while (tmp)
+      {
+        putByte('0' + tmp % 10);
+        tmp /= 10;
+      }
+      putByte('P');
+      tmp = currentPosition * 10000;
+      if (tmp < 0)
+      {
+        putByte('-');
+        tmp *= -1;
+      }
+      while (tmp)
+      {
+        putByte('0' + tmp % 10);
+        tmp /= 10;
+      }
+
+      putByte('\n');
+
       lastLogging += loggingInterval;
     }
 
